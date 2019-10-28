b399a09b65ebec9fc53aa92af7b13c59ace5cb46
Tianxiang from Dell5510 <tsu@slb.com>
2019-10-28 16:48:26 -0400

    init commit

--- facerec_tsu_webcam.py
+++ facerec_tsu_webcam.py
@@ -2,234 +2,22 @@ import face_recognition
 import cv2
 import numpy as np
 import glob
+import sys
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-
-
-# Open Camera
-PORT = 1
-# PORT = 'http://100.106.104.82:4747/mjpegfeed'
-# video_capture = cv2.VideoCapture(0)
-# video_capture = cv2.VideoCapture('http://10.0.0.173:4747/mjpegfeed')
-video_capture = cv2.VideoCapture(PORT)
-
-attemptOpenCam = 0
-while(not video_capture.isOpened()):
-   attemptOpenCam += 1
-   if(attemptOpenCam == 1):
-      print('Camera not detected yet, please be patient...')
-   video_capture.release()
-   cv2.destroyAllWindows()
-   video_capture = cv2.VideoCapture(PORT)
-print('Camera is on!')
-
-
-## Create Library
-# image = cv2.imread("tianxiang/tianxiang.jpeg")
-# face_locations = face_recognition.face_locations(image)
-# tianxiang_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-# print tianxiang_face_encoding.shape
-
-tianxiang_face_encoding = []
-for filename in glob.glob('tianxiang/*'):
+lucas_face_encoding = []
+for filename in glob.glob('raw_imgs/*'):
+   print(filename)
    image = cv2.imread(filename)
    face_locations = face_recognition.face_locations(image)
-   tianxiang_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# image = cv2.imread("ying/ying.jpg")
-# face_locations = face_recognition.face_locations(image)
-# ying_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-#ying_face_encoding = []
-#for filename in glob.glob('ying/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   ying_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# image = cv2.imread("meng/meng.jpg")
-# face_locations = face_recognition.face_locations(image)
-# meng_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-#meng_face_encoding = []
-#for filename in glob.glob('meng/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   meng_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-
-# image = cv2.imread("xianjun/xianjun.jpg")
-# face_locations = face_recognition.face_locations(image)
-# xianjun_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-#xianjun_face_encoding = []
-#for filename in glob.glob('xianjun/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   xianjun_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-
-# image = cv2.imread("muhannad/muhannad.jpg")
-# face_locations = face_recognition.face_locations(image)
-# muhannad_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-muhannad_face_encoding = []
-for filename in glob.glob('muhannad/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   muhannad_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# image = cv2.imread("jeremy/jeremy2.jpg")
-# face_locations = face_recognition.face_locations(image)
-# jeremy_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-jeremy_face_encoding = []
-for filename in glob.glob('jeremy/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   jeremy_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# image = cv2.imread("miranda/miranda.jpg")
-# face_locations = face_recognition.face_locations(image)
-# miranda_face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-
-#miranda_face_encoding = []
-#for filename in glob.glob('miranda/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   miranda_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-sepand_face_encoding = []
-for filename in glob.glob('sepand/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   sepand_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# anastasia_face_encoding = []
-# for filename in glob.glob('anastasia/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   anastasia_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# jan_face_encoding = []
-# for filename in glob.glob('jan/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   jan_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# demos_face_encoding = []
-# for filename in glob.glob('demos/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   demos_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# cecilia_face_encoding = []
-# for filename in glob.glob('cecilia/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   cecilia_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-# chris_face_encoding = []
-# for filename in glob.glob('chris/*'):
-#    image = cv2.imread(filename)
-#    face_locations = face_recognition.face_locations(image)
-#    chris_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-gavin_face_encoding = []
-for filename in glob.glob('gavin/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   gavin_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-matt_face_encoding = []
-for filename in glob.glob('matt/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   matt_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-rich_face_encoding = []
-for filename in glob.glob('rich/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   rich_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-rod_face_encoding = []
-for filename in glob.glob('rod/*'):
-   image = cv2.imread(filename)
-   face_locations = face_recognition.face_locations(image)
-   rod_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-#alejandro_face_encoding = []
-#for filename in glob.glob('alejandro/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   alejandro_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-#nicolas_face_encoding = []
-#for filename in glob.glob('nicolas/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   nicolas_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
-#walid_face_encoding = []
-#for filename in glob.glob('walid/*'):
-#   image = cv2.imread(filename)
-#   face_locations = face_recognition.face_locations(image)
-#   walid_face_encoding.append(face_recognition.face_encodings(image, face_locations)[0])
-
+   lucas_face_encoding.append(face_recognition.face_encodings(image,face_locations)[0])
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    tianxiang_face_encoding,
-    #ying_face_encoding,
-    #meng_face_encoding,
-    #xianjun_face_encoding,
-    muhannad_face_encoding,
-    jeremy_face_encoding,
-    #miranda_face_encoding,
-    sepand_face_encoding,
-    #anastasia_face_encoding,
-    #alejandro_face_encoding,
-    #nicolas_face_encoding,
-    #walid_face_encoding,
-    #jan_face_encoding,
-    #demos_face_encoding,
-    #cecilia_face_encoding,
-    #chris_face_encoding,
-    gavin_face_encoding,
-    matt_face_encoding,
-    rich_face_encoding,
-    rod_face_encoding
+    lucas_face_encoding
 ]
 
 known_face_names = [
-    "Tianxiang",
-    #"Ying",
-    #"Meng",
-    #"Xianjun",
-    "Muhannad",
-    "Jeremy",
-    #"Miranda",
-    "Sepand",
-    #"Anastasia",
-    #"Alejandro",
-    #"Nicolas",
-    #"Walid",
-    #"Jan",
-    #"Demos",
-    #"Cecilia",
-    #"Chris",
-    "Gavin",
-    "Matt",
-    "Rich",
-    "Rod"
+    "Lucas"
 ]
 
 # Initialize some variables
@@ -237,29 +25,17 @@ face_locations = []
 face_encodings = []
 face_names = []
 
-#process_this_frame = True
-numInGroup = 10
-frameID = -1
-cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Video',640,480)
 font = cv2.FONT_HERSHEY_DUPLEX
 
 while True:
-    frameID += 1
-
     # Grab a single frame of video
-    ret, frame = video_capture.read()
-
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    # small_frame = frame
+    frame = cv2.imread('test/2019-03-16 164209.jpg')
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    # rgb_small_frame = small_frame[:, :, ::-1]
     rgb_small_frame = frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if(frameID % numInGroup == 0):
+    if True:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
